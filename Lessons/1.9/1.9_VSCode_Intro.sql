4e6b1ba0632eadd0ea332d0596b67a8e6176009b
Shikhar Bansal <shikhar1551998@gmail.com>
2026-05-23 00:13:28 +0530

    Initial commit

---
+++ Lessons/1.9/1.9_VSCode_Intro.sql
@@ -0,0 +1,12 @@
+Select 42 as answer;
+select 
+job_id,
+job_title_short,
+salary_year_avg,
+company_id
+from
+job_postings_fact
+limit 10;
+
+ select * from information_schema.table_constraints
+ where table_catalog='data_jobs';
